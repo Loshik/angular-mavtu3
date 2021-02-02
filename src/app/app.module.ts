f225db83e71882dc7ce05da372e430b485a4ad25
Loshik <loshik@gmail.com>
2021-02-02 13:42:03 -0500

    Configure AppModule to use HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
@@ -14,6 +15,7 @@ import { CartComponent } from "./cart/cart.component";
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
@@ -29,8 +31,7 @@ import { CartComponent } from "./cart/cart.component";
     ProductDetailsComponent,
     CartComponent
   ],
-  bootstrap: [AppComponent],
-  providers: [CartService]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
 
